7dcde4615cce4e9cfa57c1a04607c40701ef60aa
mhdarieff <ariefm0311@@gmail.com>
2026-08-15 06:11:08 +0700

    fix: mengembalikan kecepatan scroll lenis ke normal

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -4,14 +4,8 @@ import { ReactLenis } from "@studio-freight/react-lenis";
 
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
   return (
-    <ReactLenis 
-      root 
-      options={{ 
-        lerp: 0.08, // Tingkat kelembutan (semakin kecil = semakin mulus/lambat)
-        duration: 1.5, // Durasi efek scroll
-        smoothWheel: true, 
-      }}
-    >
+    // Kita hapus properti 'options' agar Lenis menggunakan kecepatan normal bawaannya
+    <ReactLenis root>
       {/* @ts-ignore */}
       {children}
     </ReactLenis>
